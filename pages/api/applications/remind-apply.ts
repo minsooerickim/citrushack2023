62decd6cc2422e9fe16b5f1f7b3113ec97fc7369
Minsoo Kim <minsooerickim@gmail.com>
2022-11-11 23:07:11 -0800

    add remind-apply endpoint

---
+++ pages/api/applications/remind-apply.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { sendEmail } from '@/lib/sendgrid'
+import { getSession } from 'next-auth/react'
+
+export default async function remindApply(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({ req })
+  if (session && session.user.admin) {
+    const { users } = req.body
+    
+    // send email notification to remind user to apply
+    for (let i = 0; i < users.length; i++) {
+      await sendEmail({
+        email: users[i].email,
+        template_id: process.env.SENDGRID_APPLICATION_APPLY_REMINDER_EMAIL_TEMPLATE_ID,
+        name: '',
+        members: '',
+        invite_code: '',
+        newcomer: ''
+      })
+    }
+  
+    res.status(200).json({})
+  }
+  else {
+    res.status(401).json({})
+  }
+}
