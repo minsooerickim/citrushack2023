8e0ff234e9c121b02ba265284166c2816d4d9327
Minsoo Kim <minsooerickim@gmail.com>
2022-11-23 00:15:56 -0800

    add nav test

---
+++ cypress/e2e/nav.cy.ts
@@ -0,0 +1,8 @@
+describe('Navigation', () => {
+  it('should navigate back to the home page', () => {
+    cy.visit(Cypress.env('host') + '404', {failOnStatusCode: false})
+    cy.request({url: '/404', failOnStatusCode: false}).its('status').should('equal', 404)
+    cy.contains("Go Back to Homepage"). click()
+    cy.url().should('eq', Cypress.env('host'))
+  })
+})
