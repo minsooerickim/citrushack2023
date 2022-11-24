4eb358bd65c48c82a07bba7f49807a59071c433d
Minsoo Kim <minsooerickim@gmail.com>
2022-11-23 20:00:47 -0800

    add scafolding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,55 @@
-import Head from 'next/head';
+// import { useState, useEffect } from 'react'
+// import Link from 'next/link'
+// import { motion } from 'framer-motion'
+import { Element } from 'react-scroll'
+import { Page } from '@/components/Page'
+// import Modal from '@/components/Modal'
+import Landing from '@/pages/sections/landing'
+import About from '@/pages/sections/about'
+import Tracks from '@/pages/sections/tracks'
+import Support from '@/pages/sections/support'
+import Sponsors from '@/pages/sections/sponsors'
+import Staff from '@/pages/sections/staff'
+import Faq from '@/pages/sections/faq'
+import Schedule from '@/pages/sections/schedule'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Citrus Hack 2023</title>
-        <meta name="Citrus Hack 2023" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  );
+    <Page>
+      <Element name='Home' className='flex justify-center w-full bg-gradient-to-b from-primary to-accent'>
+        <span className='flex justify-center px-4 w-full bg-pattern bg-repeat bg-contain'>
+          <Landing />
+        </span>
+      </Element>
+      <Element name='Schedule' className='relative flex justify-center px-4 w-full bg-gradient-to-b from-accent-secondary to-accent'>
+        <Schedule />
+      </Element>
+      <Element name='About' className='relative flex justify-center w-full bg-gradient-to-b from-primary to-secondary'>
+        <span className='flex justify-center px-4 w-full bg-pattern bg-repeat bg-contain 2xl:bg-cover'>
+          <About />
+        </span>
+      </Element>
+      <Element name='Tracks' className='flex justify-center w-full bg-primary'>
+        <span className='flex justify-center px-4 w-full bg-pattern bg-repeat bg-contain 2xl:bg-cover'>
+          <Tracks />
+        </span>
+      </Element>
+      <Element name='Support' className='flex justify-center w-full bg-gradient-to-b from-accent to-card'>
+        <span className='flex justify-center px-4 w-full bg-pattern bg-repeat bg-contain 2xl:bg-cover'>
+          <Support />
+        </span>
+      </Element>
+      <Element name='Sponsors' className='flex justify-center px-4 w-full bg-gradient-to-b from-accent-secondary to-accent'>
+        <Sponsors />
+      </Element>
+      <Element name='Staff' className='flex justify-center w-full bg-primary'>
+        <span className='flex justify-center px-4 w-full bg-pattern bg-repeat bg-contain'>
+          <Staff />
+        </span>
+      </Element>
+      <Element name='FAQ' className='flex justify-center px-4 w-full bg-gradient-to-b'>
+        <Faq />
+      </Element>
+    </Page>
+  )
 }
--- pages/sections/landing.tsx
+++ pages/sections/landing.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
-import { useTheme } from 'next-themes';
 import { motion } from 'framer-motion';
 import { useSession } from 'next-auth/react';
 import { CountdownWrapper } from '@/components/Countdown';
@@ -12,7 +10,6 @@ import SignupCounter from '@/components/SignupCounter';
 
 export default function Landing() {
   const [mounted, setMounted] = useState(false);
-  const { theme } = useTheme();
   const { data: session, status } = useSession();
   const [signinModalOpen, setSigninModalOpen] = useState(false);
 
@@ -27,45 +24,10 @@ export default function Landing() {
   return (
     <>
       <section className="relative flex flex-col-reverse w-full h-screen min-h-[48rem] md:min-h-[60rem] xl:max-w-[68rem] 2xl:max-w-[80rem] justify-center items-center my-40 lg:my-0 mb-20 md:mb-0 gap-6">
-        <motion.div
-          animate={{ y: [0, -15, 0] }}
-          transition={{
-            repeat: Infinity,
-            duration: 5,
-          }}
-          className="lg:self-end max-w-[30rem] lg:max-w-[80rem] lg:w-5/12 xl:w-1/2 2xl:w-7/12 transform-gpu"
-        >
-          <Image
-            src={
-              theme == 'light'
-                ? '/assets/island-light.svg'
-                : '/assets/island-dark.svg'
-            }
-            width={1712.57}
-            height={1734.07}
-            quality={50}
-            priority={Boolean(true)}
-            objectFit="contain"
-          />
-        </motion.div>
         <div className="lg:absolute flex flex-col left-4 2xl:left-0 items-center">
           <div className="flex flex-col max-w-xl">
-            <div className="flex flex-col sm:flex-row items-center text-center sm:text-left sm:mb-10">
-              <Image
-                src={'/assets/logo.svg'}
-                width={150}
-                height={150}
-                objectFit="contain"
-              />
-              <div>
-                <h1 className="font-black uppercase">Citrus Hack</h1>
-                <h3 className="font-bold text-highlight-dark">
-                  April 2-3, 2022
-                </h3>
-              </div>
-            </div>
           </div>
-          <CountdownWrapper date="2022-04-02T16:00:00Z" />
+          <CountdownWrapper date="2023-04-02T16:00:00Z" />
           {/* <p className='max-w-lg italic text-center font-medium'>
             In-person sign-ups will close today at 5:30 PM PST, as we are reaching maximum capacity
           </p> */}
