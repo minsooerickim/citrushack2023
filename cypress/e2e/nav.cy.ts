d2d8903c2bc067c8b62e4ed92e4c80beeb63efa4
Minsoo Kim <minsooerickim@gmail.com>
2022-11-23 19:19:51 -0800

    update nav file

--- cypress/e2e/nav.cy.ts
+++ cypress/e2e/nav.cy.ts
@@ -1,9 +1,10 @@
 describe('Navigation', () => {
   it('should navigate back to the home page', () => {
-    cy.visit(Cypress.env('host') + '404', {failOnStatusCode: false})
+    // cy.visit(Cypress.env('host') + '404', {failOnStatusCode: false})
+    cy.visit('http://localhost:3000/', {failOnStatusCode: false})
     cy.request({url: '/404', failOnStatusCode: false}).its('status').should('equal', 404)
     cy.contains("Go Back to Homepage"). click()
-    cy.url().should('eq', Cypress.env('host'))
+    cy.url().should('eq','http://localhost:3000/')
   })
 })
 
