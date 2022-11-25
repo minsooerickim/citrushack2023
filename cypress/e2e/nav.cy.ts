a5cd6081e05435cfb8f048b0d337355d8da9f6eb
Minsoo Kim <minsooerickim@gmail.com>
2022-11-24 22:48:26 -0800

    modify expected baseurl

--- cypress/e2e/nav.cy.ts
+++ cypress/e2e/nav.cy.ts
@@ -3,8 +3,8 @@ describe('Navigation', () => {
     // cy.visit(Cypress.env('host') + '404', {failOnStatusCode: false})
     cy.visit('/404', {failOnStatusCode: false})
     cy.request({url: '/404', failOnStatusCode: false}).its('status').should('equal', 404)
-    cy.contains("Go Back to Homepage"). click()
-    cy.url().should('eq', Cypress.config().baseUrl)
+    cy.contains("Go Back to Homepage"). click()    
+    cy.url().should('eq', Cypress.config().baseUrl + '/')
   })
 })
 
