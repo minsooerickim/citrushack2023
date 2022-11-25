76087d8f2826ac45a8bc801a75d03710f87f2419
Minsoo Kim <minsooerickim@gmail.com>
2022-11-24 21:07:55 -0800

    fix vercel build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,11 @@ import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import { UserBar } from '@/components/UserBar';
 import { MLHBanner } from '@/components/MLHBanner';
+import { Session } from 'next-auth';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps<{
+  session: Session;
+}>) {
   return (
     <SessionProvider session={pageProps.session} refetchInterval={5 * 60}>
       <ThemeProvider enableSystem={false}>
