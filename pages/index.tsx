947b14c004a30e35a096d2b6930692f11a48bf32
Minsoo Kim <minsooerickim@gmail.com>
2022-12-01 21:50:17 -0800

    update coming soon page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
+import ExternalLink from '@/components/ExternalLink';
 import { Page } from '@/components/Page';
 
 export default function Home() {
   return (
     <Page>
-      <div>
-        <h1>coming soon!</h1>
+      <div className='flex flex-col text-center'>
+        <h1>Citrus Hack 2023 Coming Soon!</h1>
+        <p>
+          Feel free to email us at{' '}
+          <ExternalLink
+            name="citrushack@gmail.com"
+            link="mailto:citrushack@gmail.com"
+          />{' '}
+          if you have any more questions.
+        </p>
       </div>
     </Page>
   );
