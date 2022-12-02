27363165e96f99ae2a9e4b7a30be54d290fa7197
Minsoo Kim <minsooerickim@gmail.com>
2022-12-01 22:06:48 -0800

    remove nav test for cypress

--- cypress/e2e/nav.cy.ts
+++ cypress/e2e/nav.cy.ts
@@ -1,13 +1,2 @@
-describe('Navigation', () => {
-  it('should navigate back to the home page', () => {
-    // cy.visit(Cypress.env('host') + '404', {failOnStatusCode: false})
-    cy.visit('/404', { failOnStatusCode: false });
-    cy.request({ url: '/404', failOnStatusCode: false })
-      .its('status')
-      .should('equal', 404);
-    cy.contains('Go Back to Homepage').click();
-    cy.url().should('eq', Cypress.config().baseUrl + '/');
-  });
-});
 
 export {};
