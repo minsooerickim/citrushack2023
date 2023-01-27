e011ccf438e146d45f285e59c6004af7e7b21fd5
NovaSagittarii <novasagittarii@gmail.com>
2023-01-27 12:06:14 -0700

    test toggle theme button

---
+++ cypress/e2e/toggle-theme.cy.ts
@@ -0,0 +1,18 @@
+
+describe('Toggle theme button', () => {
+	const checkTheme = (theme) => {
+		cy.get('html')
+    	.should('have.attr', 'data-theme')
+    	.and('equal', theme)
+    cy.get('html')
+    	.should('have.css', 'color-scheme')
+    	.and('equal', theme)
+	}
+  it('check document data-theme and color-scheme style', () => {
+    cy.visit('127.0.0.1:3000')
+
+    checkTheme('light')
+    cy.get('span.md\\:block > button:has(svg)').click()
+    checkTheme('dark')
+  })
+})
