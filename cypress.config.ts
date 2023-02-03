29c468f3f5b77d08ac08459170e5ee616204e542
NovaSagittarii <novasagittarii@gmail.com>
2023-02-02 23:07:19 -0700

    get auth stub working

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from "cypress";
+require('dotenv').config({path: './.env.development'});
 
 export default defineConfig({
   projectId: 'krfkwu',
@@ -7,6 +8,11 @@ export default defineConfig({
     baseUrl: 'http://localhost:3000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      // https://github.com/cypress-io/cypress-example-recipes/blob/master/examples/server-communication__env-variables/cypress.config.js
+      config.env = config.env || {}
+      config.env.SESSION_TOKEN = process.env.CYPRESS_SESSION_TOKEN
+      console.log('extended config.env with process.env.{SESSION_TOKEN}', config.env)
+      return config
     },
   },
 });
--- cypress/e2e/apply-page.cy.ts
+++ cypress/e2e/apply-page.cy.ts
@@ -4,13 +4,18 @@ describe('Verify /apply is working', () => {
 		cy.get('html').should('have.css', 'color-scheme').and('equal', theme);
 	};
 	it('check document data-theme and color-scheme style', () => {
+		cy.login();
+		cy.visit('/');
+		cy.wait("@session");
+
 		cy.visit('/apply');
-		cy.get('div > button').contains('Sign In').click();
-		cy.get('input[name=email]').type('testaccount@test.test');
-		cy.get('button[type=submit]').contains('Sign In With Email').click();
-		cy.get('button > span').contains('Sign in with Google').parent().click();
-		cy.get('input[type=email]').type(process.env.TESTACCOUNT_EMAIL);
-		cy.get('div > span').contains('Next').parent().click();
+		
+		// cy.get('div > button').contains('Sign In').click();
+		// cy.get('input[name=email]').type('testaccount@test.test');
+		// cy.get('button[type=submit]').contains('Sign In With Email').click();
+		// cy.get('button > span').contains('Sign in with Google').parent().click();
+		// cy.get('input[type=email]').type(process.env.TESTACCOUNT_EMAIL);
+		// cy.get('div > span').contains('Next').parent().click();
 
 	});
 });
