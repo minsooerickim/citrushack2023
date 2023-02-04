0b0a88e7711d811666dbaff614f51a3949c641f3
Minsoo Kim <minsooerickim@gmail.com>
2023-02-04 00:28:22 -0800

    add qr code

---
+++ components/QR.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import QRCode from "react-qr-code";
+{/* <QRCode value="10.191.130.206:3000/users/63dc644a883e6d7080d03f1aloc" /> */}
+export default function QR() {
+return (
+<QRCode value="citrushack.com" />
+)     
+}
