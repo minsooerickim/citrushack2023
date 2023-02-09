6a7fd3da7c4ca1b7e2eae8972a0caa65682f0271
Minsoo Kim <minsooerickim@gmail.com>
2023-02-09 01:18:59 -0800

    return all fields in user data

--- lib/getUserData.ts
+++ lib/getUserData.ts
@@ -4,14 +4,11 @@ export async function getUserData(uid) {
     console.log(uid)
     const db = (await clientPromise).db(process.env.MONGODB_DB);
 
-    const userData = {}
+    // const userData = {}
+    
     const user = await db
         .collection('users')
         .find({ uid: { $eq: uid } })
         .toArray();
-
-    userData['uid'] = uid;
-    userData['email'] = user[0]['email']
-
-    return userData
+    return JSON.stringify(user);
 }
--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,16 +1,62 @@
-import { Page } from '@/components/Page';
+import { ProtectedPage } from '@/components/Page';
 import QR from '@/components/QR';
 import { getAllUserIds } from '@/lib/getAllUserIds';
 import { getUserData } from '@/lib/getUserData';
+import { useSession } from 'next-auth/react';
 
 export default function Info({ userData }) {
-  console.log(userData);
+  const { data: session, status } = useSession();
+  const json_obj = JSON.parse(userData);
+  const { uid, email, shirtSize, ...other } = json_obj[0];
+
+  // {
+  //   _id: new ObjectId("63e4b1f210e022bb585f31b3"),
+  //   name: { first: 'Minsoo', last: 'Kim' },
+  //   email: 'minsooerickim@gmail.com',
+  //   image: 'https://lh3.googleusercontent.com/a/AEdFTp5iWOrFskrAWo5jK4aPvfWZ60ceajGGN_zbPKAv1Q=s96-c',
+  //   emailVerified: null,
+  //   MLHAcknowledgement: false,
+  //   admin: false,
+  //   appliedAt: 2023-02-09T08:42:45.055Z,
+  //   applied_after_limit: false,
+  //   criteriaMet: false,
+  //   firstTimeHacker: 'Yes',
+  //   foodPreference: 'Meat',
+  //   gender: 'Nonbinary',
+  //   gid: '',
+  //   grade: '2nd Year Undergraduate',
+  //   graduationDate: '2023-02-16',
+  //   major: 'Computer Science',
+  //   participation: 'In-Person',
+  //   phoneNumber: '',
+  //   qualified: '',
+  //   race: 'Hispanic or Latino',
+  //   school: 'Abertay University',
+  //   shirtSize: 'M',
+  //   uid: '5ckOzWDbhHIKCMusBlqXS'
+  // }
+
+  console.log(other);
+  // console.log('hello' + JSON.parse(userData));
   return (
-    <Page>
-      <p>{userData.uid}</p>
-      <p>{userData.email}</p>
+    // only admins and the user themselves can see this page
+    <ProtectedPage
+      title="user info"
+      restrictions={['signin', 'self']}
+      uid={uid}
+    >
+      {/* user can only see the QR code */}
       <QR />
-    </Page>
+
+      {/* only admins can see actions and additional info */}
+      {status === 'authenticated' && session.user.admin && (
+        <div>
+          <p>{uid}</p>
+          <p>{email}</p>
+          <p>shirt size: {shirtSize}</p>
+        </div>
+      )}
+    </ProtectedPage>
   );
 }
 
@@ -26,7 +72,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // Fetch necessary data for the blog post using params.id
-  const userData = await getUserData(params.id);
+  const userData = JSON.parse(JSON.stringify(await getUserData(params.id)));
   return {
     props: {
       userData,
