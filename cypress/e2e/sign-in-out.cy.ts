f331b61e2d7201e0dcc1b73b6611fd1f9b883237
Thomas LI <tli186@wch117-06.cs.ucr.edu>
2023-02-08 20:05:16 -0800

    check /api/auth/signout is called

---
+++ cypress/e2e/sign-in-out.cy.ts
@@ -0,0 +1,18 @@
+describe('Check sign out button (sign in is mocked)', () => {
+	it('make sure log out succeeds', () => {
+		// setup spying before request happens
+		cy.server();
+		cy.intercept({
+			method: 'POST',
+			url: '/api/auth/signout',
+		}).as('post');	
+
+		cy.mock();
+		cy.login();
+		cy.visit('/');
+		cy.wait('@session');
+		cy.get('div.relative > button').click();
+		cy.get('button.group:contains("Sign Out")').click();
+		cy.wait('@post');
+	});
+});
