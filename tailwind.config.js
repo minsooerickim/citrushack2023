1745d678945fab943ed174b89db7602c40f9664a
Mariam Golwalla <mgolw001@ucr.edu>
2023-02-18 19:56:09 -0800

    colorpallete added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   mode: 'jit',
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     screens: {
-      'xs': '420px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      xs: '420px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
       '2xl': '1536px',
     },
     extend: {
@@ -15,39 +18,27 @@ module.exports = {
         pattern: 'var(--pattern)',
       },
       fontFamily: {
-        baloo_semi_bold: ["BalooPaaji2Bold"],
-        baloo_regular: ["BalooPaaji2Reg"],
-        baloo_medium: ["BalooPaaji2Medium"],
-        baloo_bold: ["BalooPaaji2Bold"]
+        baloo_semi_bold: ['BalooPaaji2Bold'],
+        baloo_regular: ['BalooPaaji2Reg'],
+        baloo_medium: ['BalooPaaji2Medium'],
+        baloo_bold: ['BalooPaaji2Bold'],
       },
       colors: {
-        judge: 'var(--judge)',
-        opposite: 'var(--opposite)',
-        transparent: 'var(--transparent)',
-        text: 'var(--text)',
-        buttons: 'var(--buttons)',
-        card: 'var(--card)',
-        primary: 'var(--primary)',
-        secondary: 'var(--secondary)',
-        third: 'var(--third)',
-        card: 'var(--card)',
-        overlay: 'var(--overlay)',
-        highlight: {
-          DEFAULT: 'var(--highlight)',
+        citrus: {
+          sky: '#A3DEFF',
+          bubble: '#B1E3FF',
+          darkgreen: '#2F5525',
+          lightgreen: '#69C063',
+          brown: '#71461F',
+          gold: '#EF9B1F',
+          bluegray: '#514F66',
+          gray: '#5F5F5F',
+          white: '#FFFFFF',
+          darksky: '#2C314F',
+          darkbubble: '#373A5E',
         },
-        sub: {
-          DEFAULT: 'var(--sub)',
-          secondary: 'var(--sub-secondary)'
-        },
-        from: 'var(--from)',
-        via: 'var(--via)',
-        via2: 'var(--via2)',
-        to: 'var(--to)',
-        waveFrom: 'var(--wave-from)',
-        waveVia: 'var(--wave-via)',
-        waveTo: 'var(--wave-to)',
       },
     },
   },
   plugins: [],
-}
+};
