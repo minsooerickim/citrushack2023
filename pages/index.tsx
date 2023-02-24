0979fcc1d4033b5acc7982d1a0a16048ad02d5c4
Minsoo Kim <minsooerickim@gmail.com>
2023-02-23 19:40:30 -0800

    run prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import ExternalLink from '@/components/ExternalLink';
 import { Page } from '@/components/Page';
 import Image from 'next/image';
-import logo from '../public/assets/logo.svg'
+import logo from '../public/assets/logo.svg';
 
 export default function Home() {
   return (
     <Page>
       <div className="flex flex-col text-center h-screen justify-center items-center">
-        <span className='pr-6'>
-        <Image src={logo} width={100} height={100} alt=''/>
+        <span className="pr-6">
+          <Image src={logo} width={100} height={100} alt="" />
         </span>
         <h1>Citrus Hack 2023 Coming Soon!</h1>
-        <p className=' text-black/[.5]'>
+        <p className=" text-black/[.5]">
           Feel free to email us at{' '}
           <ExternalLink
             name="citrushack@gmail.com"
