88828feada294aa533b037a4077a7638b4f324f9
mika shanela <mika1shanela@gmail.com>
2023-02-25 14:26:23 -0800

    tinkering

--- pages/sections/about.tsx
+++ pages/sections/about.tsx
@@ -5,7 +5,7 @@ import Postcard from '../../public/assets/postcard.svg';
 export default function About() {
   return (
     <section className="flex flex-col w-full h-full my-12 max-h-[60rem] justify-center items-center">
-      <div className="flex justfify-end items-end translate-x-18 left-20 lg:mb-20">
+      <div className="flex justfify-end items-end translate-x-18 left-20 mb-10 lg:mb-20">
         <div className="w-screen sm:translate-x-12 lg:translate-x-20 xl:translate-x-28 px-4">
           <Image src={Postcard} alt="Postcard" />
         </div>
