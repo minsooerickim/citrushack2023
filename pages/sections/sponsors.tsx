1f1493b060b4a83e6cfcdad35cb053e8ecb75f9f
Minsoo Kim <minsooerickim@gmail.com>
2023-02-28 12:26:25 -0800

    add sponsor from cutie

--- pages/sections/sponsors.tsx
+++ pages/sections/sponsors.tsx
@@ -1,20 +1,20 @@
-// import { SponsorsGrid } from '@/components/Sponsors'
-// import { ButtonLink } from '@/components/ButtonLink'
+import { SponsorsGrid } from '@/components/Sponsors'
+import { ButtonLink } from '@/components/ButtonLink'
 
 export default function Sponsors() {
   return (
-    <section className="flex flex-col w-full h-full max-w-[60rem] my-12 lg:my-0 justify-center items-center">
-      <h1 className="text-center">Our Sponsors</h1>
-      <p className="mb-12 text-center">
+    <section className='flex flex-col w-full h-full max-w-[60rem] my-12 lg:my-0 justify-center items-center'>
+      <h1 className='text-center'>Our Sponsors</h1>
+      <p className='mb-12 text-center'>
         Thank you to all of those who made this hackathon possible!
       </p>
-      {/* <ButtonLink
+      <ButtonLink
         primary
         link='/citrushack-2022-sponsorship-packet.pdf'
         label='Sponsor Us'
         external
       />
-      <SponsorsGrid /> */}
+      <SponsorsGrid />
     </section>
-  );
+  )
 }
