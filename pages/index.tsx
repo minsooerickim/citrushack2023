05b2f4fefa025c310f676f9933aa33b3dc55587c
Minsoo Kim <minsooerickim@gmail.com>
2023-03-07 21:29:58 -0800

    run prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
       <div className="flex flex-col md:grid md:grid-cols-2 text-center h-screen justify-center items-center px-6 space-x-2">
         <div className=" col-span-1 px-2">
           <div className="py-4">
-            <div className="pb-4 text-2xl md:text-6xl font-Nunito font-bold text-white">Citrus Hack 2023<br></br>Registration Countdown</div>
+            <div className="pb-4 text-2xl md:text-6xl font-Nunito font-bold text-white">
+              Citrus Hack 2023<br></br>Registration Countdown
+            </div>
             <CountdownWrapper date="2023-04-03T01:00:00" />
           </div>
 
