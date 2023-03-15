c5d8e95aff6bdfc408d43ca05e8c1057ced8cf43
NovaSagittarii <novasagittarii@gmail.com>
2023-03-15 12:38:50 -0600

    so what if baseUrl has its end removed?

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: 'krfkwu',
   e2e: {
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       // https://github.com/cypress-io/cypress-example-recipes/blob/master/examples/server-communication__env-variables/cypress.config.js
