e5129015447373a3bc762d1efea0bcb9ae04ead7
Minsoo Kim <minsooerickim@gmail.com>
2023-03-15 11:39:09 -0700

    :sparkles: doc setup

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
+
 module.exports = {
     plugins: {
-        tailwindcss: {},
+        tailwindcss: {
+            content: ["./src/**/*.{html,js}"],
+        },
         autoprefixer: {},
     },
 }
