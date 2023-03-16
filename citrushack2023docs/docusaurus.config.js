95594647787db02cbc908592e9e0fe4cbb6c753e
Minsoo Kim <minsooerickim@gmail.com>
2023-03-16 00:36:11 -0700

    :bug: hopefully? vercel dep errors for some reason

--- citrushack2023docs/docusaurus.config.js
+++ citrushack2023docs/docusaurus.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-// @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
