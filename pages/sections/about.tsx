6bd2700b5d3e929d09c115a2283abca1e7c0b143
msclal <mika1shanela@gmail.com>
2023-03-19 22:33:45 -0700

    :white_check_mark: updated loading test for about us svgs
    bleh

--- pages/sections/about.tsx
+++ pages/sections/about.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import aboutUs from '../../public/assets/aboutUs.svg';
+import AboutUs from '../../public/assets/aboutUs.svg';
 import Postcard from '../../public/assets/postcard.svg';
 import React from 'react';
 
@@ -7,7 +7,7 @@ export default function About() {
   return (
     <section className="flex flex-col w-full h-full my-8 lg:my-14 max-h-[60rem] justify-center items-center">
       <div className="w-5/6 xl:w-2/3 hidden mb-12 lg:mb-0 sm:inline">
-        <Image src={aboutUs} alt="Postcard" />
+        <Image src={AboutUs} alt="AboutUs" />
       </div>
       <div className="w-full sm:w-5/6 sm:hidden mb-12">
         <Image src={Postcard} alt="Postcard" />
