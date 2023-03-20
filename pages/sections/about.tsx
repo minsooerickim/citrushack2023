d3c421a2d5ae38091f54f78c5452c9e5e3289c09
msclal <mika1shanela@gmail.com>
2023-03-19 22:16:50 -0700

    :fire: removed dead code
    removed old about us code

--- pages/sections/about.tsx
+++ pages/sections/about.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-// import Suitcases from '../../public/assets/suitcases.svg';
 import aboutUs from '../../public/assets/aboutUs.svg';
 import Postcard from '../../public/assets/postcard.svg';
 import React from 'react';
@@ -7,15 +6,12 @@ import React from 'react';
 export default function About() {
   return (
     <section className="flex flex-col w-full h-full my-8 lg:my-14 max-h-[60rem] justify-center items-center">
-      {/* <div className="flex"> */}
       <div className="w-5/6 xl:w-2/3 hidden mb-12 lg:mb-0 sm:inline">
         <Image src={aboutUs} alt="Postcard" />
       </div>
       <div className="w-full sm:w-5/6 sm:hidden mb-12">
         <Image src={Postcard} alt="Postcard" />
       </div>
-
-      {/* </div> */}
     </section>
   );
 }
