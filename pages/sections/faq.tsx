4ae5d6cd7bf71c08f11acac48c27915f0a26ceff
kirv1234 <kirtanavenkat@gmail.com>
2023-03-26 13:11:13 -0700

    Fixing quick text changes

--- pages/sections/faq.tsx
+++ pages/sections/faq.tsx
@@ -4,7 +4,7 @@ import ExternalLink from '@/components/ExternalLink';
 export default function Faq() {
   return (
     <section className="flex flex-col w-full h-full my-12 lg:mt-0 max-w-[60rem] items-center">
-      <h1>FAQ</h1>
+      <h1 className="text-center text-7xl font-black w-391 h-109 not-italic items-center mt-6">FAQ</h1>
       <p className="text-center">
         If you still can&apos;t find an answer to your question, feel free to
         email us at{' '}
