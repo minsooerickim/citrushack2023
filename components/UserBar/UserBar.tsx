344d408d8e6b73edfe3dc4aa6033fe177d2b2419
Minsoo Kim <minsooerickim@gmail.com>
2023-03-28 00:42:15 -0700

    :lipstick: fix sign in modal colors

--- components/UserBar/UserBar.tsx
+++ components/UserBar/UserBar.tsx
@@ -40,7 +40,7 @@ export function UserBar() {
           <motion.button
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.995 }}
-            className="flex justify-center items-center self-center w-24 h-11 px-4 font-semibold text-lg rounded-md bg-highlight shadow cursor-pointer text-white"
+            className="flex justify-center items-center self-center w-24 h-11 px-4 font-semibold text-lg rounded-md bg-highlight shadow cursor-pointer text-white bg-purple"
             onClick={() => toggleSigninModal()}
           >
             Sign In
