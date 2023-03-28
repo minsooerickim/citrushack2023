d646391bd74835a4a61092e1efc263f566c45ce3
msclal <mika1shanela@gmail.com>
2023-03-28 00:16:49 -0700

    :lipstick: attempting to fix cutoff

--- pages/sections/landing.tsx
+++ pages/sections/landing.tsx
@@ -234,7 +234,7 @@ export default function Landing() {
 
   return (
     <>
-      <section className="relative flex flex-col-reverse w-full h-screen md:min-h-[72rem]">
+      <section className="relative flex flex-col-reverse w-full h-screen md:min-h-[65rem]">
         <div className="absolute left-1/2 -translate-x-1/2 w-full top-[27%] md:top-[25%] lg:top-[16%] flex flex-col justify-center items-center z-10 px-4 text-textcolor">
           <div className="text-center text-6xl max-[375px]:text-5xl sm:text-7xl lg:text-8xl mb-2 lg:mb-4 font-black">
             CITRUS HACK 2023
