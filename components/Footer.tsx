774ae8f1d2024889c2dd7706fba238c7e86d2d72
Minsoo Kim <minsooerickim@gmail.com>
2023-03-29 16:19:32 -0700

    :lipstick: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image';
-import bus_footer from '../public/assets/bus_footer.svg'
+import bus_footer from '../public/assets/bus_footer.svg';
 
 export default function Footer() {
-    return (
-        <Image src={bus_footer} objectFit="contain" alt="footer"/>
-    )
+  return <Image src={bus_footer} objectFit="contain" alt="footer" />;
 }
