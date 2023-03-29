1a0041dca38dc0583f25fc246907af70332dc3eb
Minsoo Kim <minsooerickim@gmail.com>
2023-03-28 23:44:22 -0700

    :lipstick: add hover color in app

--- components/Form/ApplicationForm/index.tsx
+++ components/Form/ApplicationForm/index.tsx
@@ -184,7 +184,7 @@ export function ApplicationForm() {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.995 }}
           type="submit"
-          className="w-full py-1.5 rounded bg-buttons font-semibold text-white"
+          className="w-full py-1.5 rounded bg-buttons font-semibold text-white bg-purple hover:bg-hoverPrimary"
           onClick={() => triggerErrorNotification()}
         >
           {clickedSubmitOnce ? 'Submitting...' : 'Submit'}
