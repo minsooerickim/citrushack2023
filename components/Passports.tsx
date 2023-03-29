8e5b41f1b47253dcd692b25930ee72f15bd7ac92
msclal <mika1shanela@gmail.com>
2023-03-29 14:49:18 -0700

    commmittee template

--- components/Passports.tsx
+++ components/Passports.tsx
@@ -130,12 +130,78 @@ const Passports = () => {
         ))}
       </div>
       <div className="pt-10 md:pt-20 flex flex-col justify-center">
-        <div className="flex justify-center pb-4 items-center">
-          <div className="inline bg-gradient-to-l from-opposite to-transparent w-20 xl:w-96 h-1 items-center rounded-md" />
+        <div className="flex flex-col justify-center pb-4 items-center">
           <p className="text-opposite text-lg md:text-2xl font-baloo_semi_bold text-center text-text font-semibold">
             Committee Members
           </p>
-          <div className="inline bg-gradient-to-r from-opposite to-transparent w-20 xl:w-96 h-1 items-center rounded-md" />
+          <div className="grid grid-cols-2 md:grid-cols-5 text-center text-sub font-baloo_regular justify-center gap-y-4 items-center">
+            <div>
+              <p className="text-opposite font-baloo_semi_bold">
+                Software Engineers
+              </p>
+              <p>Valerie Wong</p>
+              <p>Theo Fernandez</p>
+              <p>Andrei Dimaano</p>
+              <p>Raidah Fairooz</p>
+              <p>Nicholas Chao</p>
+            </div>
+            <div>
+              <p className="text-opposite font-baloo_semi_bold">Operations</p>
+              <p>Westin Montano</p>
+              <p>Adhithi Narayana Murthy</p>
+              <p>Eliot Lee</p>
+              <p>Ronit Bhushan</p>
+            </div>
+            {/* <div>
+              <p className="text-opposite font-baloo_semi_bold">UI/UX</p>
+              <p>Chloe Xu</p>
+              <p>Elaine Lin</p>
+              <p>Kia Nooshi</p>
+              <p>Rania Saber</p>
+              <p>Siraaj Kudtarkar</p>
+            </div> */}
+            <div>
+              <p className="text-opposite font-baloo_semi_bold">Sponsorship</p>
+              <p>Ryan Wong</p>
+              <p>Nolan Chu</p>
+              <p>Jasmita Yechuri</p>
+              <p>Freddy Song</p>
+              <p>Iliyan Alibhai</p>
+              <p>Ethan Tran</p>
+              <p>Eliot Lee</p>
+              <p>Skyler Saltos</p>
+            </div>
+            <div>
+              {/* <div> */}
+              {/* <p className="text-opposite font-baloo_semi_bold">Marketing</p>
+                <p>Natalie Jung</p>
+                <p>Elaine Lin</p>
+              </div>
+              <div className="pt-4 hidden md:block">
+                <p className="text-opposite font-baloo_semi_bold">
+                  Quality Assurance
+                </p>
+                <p>Paulian Le</p>
+              </div> */}
+              <div className="pt-4 hidden md:block">
+                <p className="text-opposite font-baloo_semi_bold">
+                  Photographer
+                </p>
+                <p>Kobe Braga</p>
+              </div>
+            </div>
+            <div className="md:hidden">
+              <p className="text-opposite font-baloo_semi_bold">
+                Quality Assurance
+              </p>
+              <p>Paulian Le</p>
+            </div>
+            <div className="col-span-2 md:hidden">
+              <p className="text-opposite font-baloo_semi_bold">Photographer</p>
+              <p>Kobe Braga</p>
+            </div>
+          </div>
+          {/* </div> */}
         </div>
       </div>
     </section>
