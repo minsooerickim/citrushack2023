ce8170f9f4e6e453442434b310a88260395fe54e
msclal <mika1shanela@gmail.com>
2023-03-30 05:05:20 -0700

    🥊 fixed footer gap

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,29 @@
 import Image from 'next/image';
-import bus_footer from '../public/assets/bus_footer.svg';
+import { useTheme } from 'next-themes';
+
+import bus_footer from '@/public/assets/bus_footer.svg';
+import bus_footer_dark from '@/public/assets/bus_footer_dark.svg';
 
 export default function Footer() {
-  return <Image src={bus_footer} objectFit="contain" alt="footer" />;
+  const { theme } = useTheme();
+
+  return (
+    <>
+      {theme === 'light' ? (
+        <Image
+          src={bus_footer}
+          alt="footer"
+          objectFit="contain"
+          layout="responsive"
+        />
+      ) : (
+        <Image
+          src={bus_footer_dark}
+          alt="footer"
+          objectFit="contain"
+          layout="responsive"
+        />
+      )}
+    </>
+  );
 }
