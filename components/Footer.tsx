9794ab2c1c8a82175f0e78584b4412503a093a99
Minsoo Kim <minsooerickim@gmail.com>
2023-03-30 00:07:36 -0700

    :bug: fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,5 +2,12 @@ import Image from 'next/image';
 import bus_footer from '../public/assets/bus_footer.svg';
 
 export default function Footer() {
-  return <Image src={bus_footer} objectFit="contain" alt="footer" />;
+  return (
+    <Image
+      src={bus_footer}
+      alt="footer"
+      layout="responsive"
+      objectFit="contain"
+    />
+  );
 }
