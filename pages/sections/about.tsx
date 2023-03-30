04984bb173cdb8a25e5b8fb7139a6fdb21ed2eee
msclal <mika1shanela@gmail.com>
2023-03-29 19:24:35 -0700

    :lipstick: updated about us

--- pages/sections/about.tsx
+++ pages/sections/about.tsx
@@ -7,10 +7,10 @@ import React from 'react';
 export default function About() {
   return (
     <section className="relative flex flex-col w-full h-full my-8 lg:my-14 max-h-[60rem] justify-center items-center">
-      <div className="w-5/6 xl:w-2/3 hidden mb-12 lg:mb-0 sm:inline z-10">
+      <div className="w-5/6 xl:w-2/3 hidden mb-12 px-4 lg:mb-0 sm:inline z-10">
         <Image src={AboutUs} alt="AboutUs" />
       </div>
-      <div className="relative w-full sm:w-5/6 sm:hidden mb-12 z-20">
+      <div className="relative w-full sm:w-5/6 px-4 sm:hidden mb-12 z-20">
         <Image src={Postcard} alt="Postcard" />
       </div>
       <div className="absolute w-full h-full z-0">
