08c21790e39af0032810cbbb788f2ff2e5ff503a
Minsoo Kim <minsooerickim@gmail.com>
2023-04-01 21:34:38 -0700

    :lipstick: minor style issue

--- components/Form/components.tsx
+++ components/Form/components.tsx
@@ -60,6 +60,7 @@ export const Input = ({
         'w-full rounded-lg focus:border-highlight focus:outline-none focus:ring-highlight file:px-4 file:py-1.5 file:rounded-full file:border-0 file:bg-highlight file:text-sm file:font-semibold file:cursor-pointer hover:cursor-pointer' +
         (type === 'date' ? 'py-1.5 ' : 'py-1 ') +
         (type === 'file' ? ' text-white ' : 'bg-secondary px-2 border-2 ') +
+        (type === 'text' ? ' text-text ' : '') +
         (errors[variable] ? 'border-red-500' : 'border-highlight')
       }
       onChange={onChange}
