dd1d9980392a08d244f36b55a1d5b8d30358b77a
Minsoo Kim <minsooerickim@gmail.com>
2023-04-17 17:51:00 -0700

    :building_construction: add resources section

--- pages/sections/resources.tsx
+++ pages/sections/resources.tsx
@@ -5,8 +5,10 @@ export default function Resources() {
   const { data: session, status } = useSession();
 
   return (
-    <section className="flex flex-col w-full h-full my-12 lg:my-0 mb-24 max-w-[60rem] justify-center items-center">
-      <h1 className="mb-16">Resources</h1>
+    <section className="flex flex-col w-full h-full my-12 lg:my-0 mb-24 max-w-[60rem] justify-center items-center z-[1000]">
+      <h1 className="text-5xl md:text-7xl font-black mb-4 md:mb-8 text-text">
+        Resources
+      </h1>
       <div className="flex flex-col gap-4 w-full">
         <ButtonLink
           primary
@@ -15,6 +17,7 @@ export default function Resources() {
           external
         />
         <ButtonLink
+          primary
           link="https://github.com/citrushack/CitrusHackResources/blob/main/README.md"
           label="GitHub"
           external
