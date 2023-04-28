010bdf308dd18ac6ca0a61da705fd16fa1ce888f
msclal <mika1shanela@gmail.com>
2023-04-27 18:07:20 -0700

    fix link colors in external and faq👎

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -4,17 +4,14 @@ interface Props {
   /** Link to external source. */
   link: string;
 }
-import { useTheme } from 'next-themes';
 
 /** Linked text to an external source. */
 export default function ExternalLink({ name, link }: Props) {
-  const { theme } = useTheme();
   return (
     <a target="_blank" rel="noreferrer noopener" href={link}>
       <span
-        className={`font-bold hover:underline cursor-pointer  ${
-          theme === 'light' ? `text-brown` : `text-gold`
-        }`}
+        className={`font-bold hover:underline cursor-pointer text-gold
+        `}
       >
         {name}
       </span>
